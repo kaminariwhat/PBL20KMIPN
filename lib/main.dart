4aff28adb57ac81a82edea05470b755d271154f3
jaheerman3312301105 <frdhn2323@gmail.com>
2024-09-26 22:42:19 +0700

    update terbaru main

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,13 @@ class MyApp extends StatelessWidget {
           title: const Text(
             appTitle,
             style: TextStyle(
-              fontSize: 20.0, // Menetapkan ukuran teks di sini
+              fontSize: 18.0, // Menetapkan ukuran teks di sini
               fontFamily: 'Poppins',
               fontWeight: FontWeight.bold,
               shadows: <Shadow>[
                       Shadow(
                         offset: Offset(2.0, 2.0),
-                        blurRadius: 2.0,
+                        blurRadius: 1.0,
                         color: Colors.black26,
                       ),
                     ],
