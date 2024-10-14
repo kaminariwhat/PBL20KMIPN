280c53db593b678d203c8ceaa73e20edaca735f9
jaheerman3312301105 <frdhn2323@gmail.com>
2024-10-14 10:13:29 +0700

    update lapor dart dropdown deskripsi

--- lib/screens/lapor.dart
+++ lib/screens/lapor.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
-const List<String> list = <String>['Jalan', 'Lampu Jalan', 'Jembatan'];
-class LaporScreen extends StatelessWidget {
+const List<String> jenis = <String>['Jalan', 'Lampu Jalan', 'Jembatan'];
+
+class LaporScreen extends StatefulWidget {
   const LaporScreen({super.key});
+
+  @override
+  _LaporScreenState createState() => _LaporScreenState();
+}
+
+class _LaporScreenState extends State<LaporScreen> {
+  String? selectedItem = 'Jalan'; 
 
   @override
   Widget build(BuildContext context) {
@@ -11,128 +19,205 @@ class LaporScreen extends StatelessWidget {
         title: const SizedBox(
           child: Row(
             children: [
-               Text.rich(
-               TextSpan(
-                text: 'Buat Laporan',
-                style: TextStyle(
-                fontFamily: "Poppins",
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              )
-            )
-            ),
+              Text.rich(
+                TextSpan(
+                  text: 'Buat Laporan',
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ],
           ),
-        )
+        ),
       ),
-
       body: Column(
         children: [
           Center(
             child: Column(
               children: [
-                const SizedBox(height: 20),
+                const SizedBox(height: 1),
                 const SizedBox(
                   width: 340,
                   height: 30,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text('Judul Pengaduan',
-                      style: TextStyle(
-                        fontFamily: "Poppins",
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10
-                      ),
+                      Text(
+                        'Judul Pengaduan',
+                        style: TextStyle(
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                        ),
                       )
                     ],
                   ),
                 ),
                 SizedBox(
-                height: 35,
-                width: 350,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color:const Color.fromARGB(255, 230, 228, 228), 
-                    borderRadius: BorderRadius.circular(5), 
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2), 
-                        blurRadius: 4, 
-                        offset: const Offset(1, 5), 
+                  height: 35,
+                  width: 350,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 230, 228, 228),
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 4,
+                          offset: const Offset(1, 5),
+                        ),
+                      ],
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Jalan berlubang di jalan raya piayu',
+                        hintStyle: TextStyle(
+                          fontFamily: "Poppins",
+                          fontSize: 10,
+                        ),
+                        contentPadding: EdgeInsets.only(
+                          top: -12,
+                          left: 9,
+                        ),
                       ),
-                    ],
-                  ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none, 
-                      hintText: 'Jalan berlubang di jalan raya piayu',
-                      hintStyle: TextStyle(
-                        fontFamily: "Poppins",
-                        fontSize: 10,
-                      ),
-                      contentPadding: EdgeInsets.only(
-                        top: -12,
-                        left: 9
-                        ), 
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const SizedBox(
-                width: 340,
+
+                const SizedBox(height: 10),
+                const SizedBox(
+                  width: 340,
                   height: 30,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text('Jenis Pengaduan',
-                      style: TextStyle(
-                        fontFamily: "Poppins",
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10
-                      ),
+                      Text(
+                        'Jenis Pengaduan',
+                        style: TextStyle(
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                        ),
                       )
                     ],
                   ),
-              ),
-              SizedBox(
-                width: 350,
-                height: 35,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color:const Color.fromARGB(255, 230, 228, 228), 
-                    borderRadius: BorderRadius.circular(5), 
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2), 
-                        blurRadius: 4, 
-                        offset: const Offset(1, 5), 
+                ),
+                SizedBox(
+                  width: 350,
+                  height: 35,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 230, 228, 228),
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 4,
+                          offset: const Offset(1, 5),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        SizedBox(
+                      width: 320,
+                      height: 35,
+                      child: DropdownButton<String>(
+                        value: selectedItem,
+                        items: jenis.map<DropdownMenuItem<String>>((String item) {
+                          return DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(item, style: const TextStyle(
+                              fontFamily: "Poppins",
+                              fontSize: 10,
+                            ),
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (String? item) {
+                          setState(() {
+                            selectedItem = item;
+                          }
+                          );
+                        },
+                      ),
+                      ),
+                      ],
+                    )
+                  ),
+                ),
+
+                const SizedBox(height: 10),
+                const SizedBox(
+                  width: 340,
+                  height: 30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Deskripsi Pengaduan',
+                        style: TextStyle(
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                        ),
                       ),
                     ],
                   ),
-                  child: SizedBox(
-                    width: 350,
-                    height: 35,
-                    child: DropdownMenu<String>(
-                    hintText: 'Jenis Pengaduan',
-                    textStyle: const TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 10
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 350,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 230, 228, 228),
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 4,
+                          offset: const Offset(1, 5),
+                        ),
+                      ],
                     ),
-                    dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
-                      return DropdownMenuEntry<String>(value: value, label: value);
-                    }).toList(),
+                    padding: const EdgeInsets.only(
+                      top: 12,
+                      right: 2),
+                    child: const TextField(
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 12,
+                      ),
+                      cursorHeight: 14,
+                      maxLines: 6,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Entah sudah selasa yang keberapa masih saja jalan itu rusak',
+                        hintStyle: TextStyle(
+                          fontFamily: "Poppins",
+                          fontSize: 12,
+                        ),
+                        contentPadding: EdgeInsets.only(
+                          top: -12,
+                          left: 9,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+
               ],
             ),
           )
         ],
       ),
-
     );
   }
 }
