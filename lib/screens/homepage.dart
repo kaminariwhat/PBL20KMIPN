5137e652c2e133f9e81167a24a307dfe760e8768
jaheerman3312301105 <frdhn2323@gmail.com>
2024-10-31 16:10:33 +0700

    update halaman utama sedikit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -26,6 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
